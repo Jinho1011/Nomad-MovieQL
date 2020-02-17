d199579eb6e03ede5fde228c253dc811a7b22e96
jinho1011 <gino9940@gmail.com>
2020-02-17 13:51:20 +0900

    Create Queries with Arguments

--- graphql/db.js
+++ graphql/db.js
@@ -32,6 +32,6 @@ export const people = [
 ]
 
 export const getById = id => {
-    const filteredPeople = people.filter(person => people.id === id)
+    const filteredPeople = people.filter(x => x.id === id)
     return filteredPeople[0]
 }
